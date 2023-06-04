6198c7dcfaef7e471c0c6f26033f305e8dcf2470
renukachaudhari19 <111777430+renukachaudhari19@users.noreply.github.com>
2023-06-04 16:13:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Munciple Corp | Home </title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="District Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"
+	/>
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--// Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="1/css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="1/css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="1/css/fontawesome-all.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+	    rel="stylesheet">
+	<!-- //Web-Fonts -->
+</head>
+
+<body>
+	<!-- banner -->
+	<div class="bannerbg-w3l">
+		<?php include "1/include/header.php"; ?>
+		<!-- banner text -->
+		<div class="banner-text-agile">
+			<div class="container">
+				<div class="banner-w3lstexts text-white text-center">
+					<h1>Municipal Corporation Complaint System</h1>
+					<h4 class="text-uppercase mt-md-3 mt-2 mb-md-4 mb-3">At your Services 24/7</h4>
+					
+				
+				</div>
+			</div>
+		</div>
+		<!-- //banner text -->
+	</div>
+	<!-- login -->
+	
+	<!-- //login -->
+	<!-- register -->
+	
+	<!--//register-->
+	<!-- //banner -->
+
+	<!-- banner-bottom -->
+	
+	<!-- //banner-bottom -->
+
+<!-- services -->
+	<div class="what-w3ls py-5" id="services">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="text-center mb-md-5 mb-4">
+				<h3 class="tittle mb-sm-2">Our Services</h3>
+				<p>We provice Services in Pace</p>
+			</div>
+			<div class="what-grids">
+				<div class="row">
+					<div class="col-md-6 what-grid1">
+						<div class="row what-top">
+							<div class="col-2 what-left">
+								<i class="fa fa-trash"></i>
+							</div>
+							<div class="col-10 what-right">
+								<h4>Garbage Management</h4>
+								<p class="mt-2">We manage local city Garbage 24/7</p>
+							</div>
+						</div>
+						<div class="row what-top my-md-5 my-4">
+							<div class="col-2 what-left">
+								<i class="fa fa-road"></i>
+							</div>
+							<div class="col-10 what-right">
+								<h4>Road  Management</h4>
+								<p class="mt-2">We take Road problems complaints  24/7</p>
+							</div>
+						</div>
+						
+					</div>
+					<div class="col-md-6 what-grid1 my-md-0 my-4">
+						<div class="row what-top">
+							<div class="col-2 what-left">
+								<i class="fa fa-bath"></i>
+							</div>
+							<div class="col-10 what-right">
+								<h4>Water Management</h4>
+								<p class="mt-2">We manage Water supply problems 24/7</p>
+							</div>
+						</div>
+						<div class="row what-top my-md-5 my-4">
+							<div class="col-2 what-left">
+								<i class="fa fa-bolt"></i>
+							</div>
+							<div class="col-10 what-right">
+								<h4>Electricity Management</h4>
+								<p class="mt-2">We manage Electricity supply problems 24/7</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //services -->
+
+	<!-- pricing -->
+	
+	<!-- middle section -->
+	<div class="middle-w3l">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-6 col-md-4 button">
+
+				</div>
+				<div class="col-lg-6 col-md-8 left-build-wthree py-5 px-sm-5 px-4">
+					<div class="py-xl-5 py-lg-3 px-xl-4">
+						<h4>About us</h4>
+						
+						<p align="justify">A municipal corporation is a type of local government in India that administers urban areas with a population of more than one million. The growing population and urbanization of various Indian cities highlighted the need for a type of local governing body that could provide services such as healthcare, education, housing and transport by collecting property taxes and administering grants from the state government.</p>
+                        <p align="justify">
+
+The Municipal corporation carries out its function through well organized divisions or departments. For example water supply and sewage disposal Undertaking, Housing Board, Education Department and Electricity Department. Each of these Departments are looked after by experienced and qualified persons.</p>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- middle section -->
+	<div class="middle-w3l">
+		<div class="container-fluid">
+			<div class="row">
+				<h3>Latest News</h3>
+				<div class="col-lg-8 col-md-8 left-build-wthree py-5 px-sm-5 px-4">
+					<div class="py-xl-5 py-lg-3 px-xl-4">
+					<a class="twitter-timeline" href="https://twitter.com/TOIIndiaNews?ref_src=twsrc%5Etfw">Tweets by TOIIndiaNews</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+
+	<!-- //middle section -->
+	<!-- //pricing -->
+<?php include "1/include/footer.php"; ?>
+
+	<!-- Js files -->
+	<!-- JavaScript -->
+	<script src="1/js/jquery-2.2.3.min.js"></script>
+	<!-- Default-JavaScript-File -->
+	<script src="1/js/bootstrap.js"></script>
+	<!-- Necessary-JavaScript-File-For-Bootstrap -->
+
+	<!-- flexSlider (for testimonials) -->
+	<link rel="stylesheet" href="1/css/flexslider.css" type="text/css" media="screen" property="" />
+	<script defer src="1/js/jquery.flexslider.js"></script>
+	<script>
+		$(window).load(function () {
+			$('.flexslider').flexslider({
+				animation: "slide",
+				start: function (slider) {
+					$('body').removeClass('loading');
+				}
+			});
+		});
+	</script>
+	<!-- //flexSlider (for testimonials) -->
+
+	<!-- stats -->
+	<script src="1/js/jquery.waypoints.min.js"></script>
+	<script src="1/js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+	<!-- //stats -->
+
+	<!-- password-script -->
+	<script>
+		window.onload = function () {
+			document.getElementById("password1").onchange = validatePassword;
+			document.getElementById("password2").onchange = validatePassword;
+		}
+
+		function validatePassword() {
+			var pass2 = document.getElementById("password2").value;
+			var pass1 = document.getElementById("password1").value;
+			if (pass1 != pass2)
+				document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+			else
+				document.getElementById("password2").setCustomValidity('');
+			//empty string means no validation error
+		}
+	</script>
+	<!-- //password-script -->
+
+	<!-- smooth scrolling -->
+	<script src="1/js/SmoothScroll.min.js"></script>
+	<!-- //smooth scrolling -->
+
+	<!-- move-top -->
+	<script src="1/js/move-top.js"></script>
+	<!-- easing -->
+	<script src="1/js/easing.js"></script>
+	<!--  necessary snippets for few javascript files -->
+	<script src="1/js/district.js"></script>
+
+	<script src="1/js/bootstrap.js"></script>
+	<!-- Necessary-JavaScript-File-For-Bootstrap -->
+	<!-- //Js files -->
+
+</body>
+
+</html>
